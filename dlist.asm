e168e013383542435ed31e287b729d776dc31b99
Ed Salisbury <ed.salisbury@gmail.com>
2022-01-17 19:14:06 -0700

    Episode 6

---
+++ dlist.asm
@@ -0,0 +1,22 @@
+* --------------------------------------- *
+* Proc: setup_screen                      *
+* Sets up the display list for the screen *
+* --------------------------------------- *
+.proc setup_screen
+blank8 = $70    ; 8 blank lines
+lms = $40	    ; Load Memory Scan
+jvb = $41	    ; Jump while vertical blank
+
+antic2 = 2      ; Antic mode 2
+antic5 = 5	    ; Antic mode 5
+
+	mwa #dlist SDLSTL
+	rts
+
+dlist
+	.byte blank8, blank8, blank8
+	.byte antic5 + lms, <screen, >screen
+	.byte antic5, antic5, antic5, antic5, antic5, antic5
+	.byte antic5, antic5, antic5, antic5, antic5
+	.byte jvb, <dlist, >dlist
+	.endp
--- main.asm
+++ main.asm
@@ -3,7 +3,7 @@
 ; Mission: EdPossible
 ; youtube.com/MissionEdPossible
 ; Assemble in MADS: mads -l -t main.asm
-; Video 5: More work with character sets
+; Video 6: Reorganization and more graphics
 
 ; ATASCII Table: https://www.atariwiki.org/wiki/attach/Atari%20ATASCII%20Table/ascii_atascii_table.pdf
 ; ATASCII 0-31 Screen code 64-95
@@ -15,86 +15,95 @@
 
 	org $2000
 
-SDLSTL = $0230  ; Display list starting address
-CHBAS  = $02f4  ; CHaracter BAse Register
-COLOR0 = $02c4	; Color for %01
-COLOR1 = $02c5  ; Color for %10
-COLOR2 = $02c6  ; Color for %11 (normal)
-COLOR3 = $02c7  ; Color for %11 (inverse)
-COLOR4 = $02c8  ; Color for %00 (background)
-
 charset = $3c00 ; Character Set
 screen = $4000  ; Screen buffer
-blank8 = $70    ; 8 blank lines
-lms = $40	    ; Load Memory Scan
-jvb = $41	    ; Jump while vertical blank
 
-antic2 = 2      ; Antic mode 2
-antic5 = 5	    ; Antic mode 5
+	setup_screen()
+	setup_colors()
+	load_gfx()
+	display_map()
 
+	jmp *
+
+	icl 'hardware.asm'
+	icl 'dlist.asm'
+	icl 'gfx.asm'
+
+* --------------------------------------- *
+* Proc: setup_colors                      *
+* Sets up colors                          *
+* --------------------------------------- *
+.proc setup_colors
 med_gray = $06
 lt_gray = $0a
 green = $c2
 brown = $22
 black = $00
 
-; Load display list
-	mwa #dlist SDLSTL
-
-; Set up character set
-	mva #>charset CHBAS
-
-	ldx #0
-loop
-	mva chars,x charset+8,x
-	inx
-	cpx #16
-	bne loop
-
-; Change colors
 	mva #med_gray COLOR0 ; %01
 	mva #lt_gray COLOR1  ; %10
 	mva #green COLOR2	 ; %11
 	mva #brown COLOR3    ; %11 (inverse)
 	mva #black COLOR4    ; %00
+	rts
+	.endp
 
-	ldy #0
-loop2
-	mva scene,y screen,y
-	iny
-	cpy #12
-	bne loop2
+* --------------------------------------- *
+* Proc: load_gfx                          *
+* Loads graphics into character set       *
+* --------------------------------------- *
+.proc load_gfx
+	mva #>charset CHBAS
 
-	jmp *
-
-; Display List
-dlist
-	.byte blank8, blank8, blank8
-	.byte antic5 + lms, <screen, >screen
-	.byte antic5, antic5, antic5, antic5, antic5, antic5
-	.byte antic5, antic5, antic5, antic5, antic5
-	.byte jvb, <dlist, >dlist
-
-
-
-scene
-	.byte 1,2,1,2
-
-chars
-	.byte %10101010
-	.byte %10100101
-	.byte %01010101
-	.byte %00000000
-	.byte %01010010
-	.byte %01010010
-	.byte %01010010
-	.byte %00000000
+	ldx #0
+loop
+	mva gfx,x charset,x
+	mva gfx+8,x charset+8,x
+	mva gfx+16,x charset+16,x
+	mva gfx+32,x charset+32,x
 	
-	.byte %01001010
-	.byte %01001010
-	.byte %00001001
-	.byte %00000000
-	.byte %10101001
-	.byte %10010101
-	.byte %01010101
-	.byte %00000000
+	inx
+	cpx #128
+	bne loop
+	rts
+	.endp
+
+* --------------------------------------- *
+* Proc: display_map                       *
+* Displays the current map                *
+* --------------------------------------- *
+.proc display_map
+	ldy #0
+loop
+	mva map,y screen,y
+	mva map+40,y screen+40,y
+	mva map+80,y screen+80,y
+	mva map+120,y screen+120,y
+	mva map+160,y screen+160,y
+	mva map+200,y screen+200,y
+	mva map+240,y screen+240,y
+	mva map+280,y screen+280,y
+	mva map+320,y screen+320,y
+	mva map+360,y screen+360,y
+	mva map+400,y screen+400,y
+	mva map+440,y screen+440,y
+
+	iny
+	cpy #40
+	bne loop
+	rts
+	
+map
+	.byte 2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,2,3,6,7,2,3,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,4,5,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,10,11,4,5,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,4,5,4,5,2,3
+	.byte 2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3
+	.endp
