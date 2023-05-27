e4a19dc8441953542dc92e4dcc7b6e4a0cdf6877
Ed Salisbury <ed.salisbury@gmail.com>
2023-05-27 06:57:31 -0600

    Added monster details table

---
+++ monster_details.asm
@@ -0,0 +1,27 @@
+    org monster_details
+    ; Name,           M, R, D, F 
+    .byte "RAT     ", 1, 0, 1, 1 
+    .byte "ZOMBIE  ", 3, 0, 3, 2 
+    .byte "SKELETON", 2, 0, 2, 2  ; Melee
+    .byte "SKELETON", 0, 2, 2, 2  ; Ranged
+    .byte "BAT     ", 3, 0, 3, 3 
+    .byte "SNAKE   ", 1, 0, 1, 1 
+    .byte "GOBLIN  ", 1, 0, 1, 1  ; Melee
+    .byte "GOBLIN  ", 0, 1, 1, 1  ; Ranged
+    .byte "ORC     ", 2, 0, 2, 2 
+    .byte "SPIDER  ", 4, 0, 4, 4 
+    .byte "GHOUL   ", 3, 0, 3, 3 
+    .byte "MIMIC   ", 9, 0, 9, 9 
+    .byte "OGRE    ", 9, 0, 9, 9 
+    .byte "SLIME   ", 13, 0, 13, 13
+    .byte "MINOTAUR", 11, 0, 11, 0, 11
+    .byte "WEREWOLF", 9, 0, 9, 9
+    .byte "SCORPION", 8, 0, 8, 8
+    .byte "OWLBEAR ", 9, 0, 9, 9
+    .byte "GHOST   ", 7, 0, 7, 7
+    .byte "DEMON   ", 10, 0, 10, 10
+    .byte "CYCLOPS ", 21, 0, 21, 21
+    .byte "WYVERN  ", 17, 0, 17, 17
+    .byte "VAMPIRE ", 22, 0, 22, 22
+    .byte "GAZER   ", 28, 28, 28, 28
+    .byte "DRAGON  ", 40, 40, 40, 40
